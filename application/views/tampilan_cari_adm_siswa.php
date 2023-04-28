d4c721a35fadbb19f58c120b42670dacd35a1ec0
Rahmadika S Setiawan <dikakewren@outlook.com>
2023-04-28 09:19:59 +0700

    Update tampilan_cari_adm_siswa.php

--- application/views/tampilan_cari_adm_siswa.php
+++ application/views/tampilan_cari_adm_siswa.php
@@ -17,27 +17,32 @@
 				<div class="row">
 					<div class="col-md mt-2">
 						<div class="card-body">
-							<table class="table table-striped table-bordered">
-								<thead>
-									<tr class="text-uppercase">
-										<th scope="col">KLS</th>
-										<th scope="col">Thn Ajrn</th>
-										<th scope="col">Blm Lns</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-										<?php
-										$no = 1;
-										foreach ($siswa as $row) {
-										?>
-											<td><?= $row['kelas']; ?></td>
-											<td><?= $row['tahun_ajaran']; ?></td>
-											<td><?= $row['sisa']; ?> Bulan</td>
-									</tr>
-								<?php } ?>
-								</tbody>
-							</table>
+							<div class="table-responsive">
+								<table class="table">
+									<table class="table table-striped table-bordered">
+										<thead>
+											<tr class="text-uppercase">
+												<th scope="col">KLS</th>
+												<th scope="col">Lns</th>
+												<th scope="col">Blm Lns</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<?php
+												$no = 1;
+												foreach ($siswa as $row) {
+												?>
+													<td><?= $row['kelas']; ?></td>
+													<td><?= $row['jumlah_bulan']; ?> Bulan</td>
+													<td><?= $row['sisa']; ?> Bulan</td>
+											</tr>
+										<?php } ?>
+										</tbody>
+									</table>
+								</table>
+							</div>
+
 						</div>
 					</div>
 				</div>
